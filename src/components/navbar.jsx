9a9c49bf3ac8f68eb91102f432ec68d8aecd9e39
Lenin Paulino <leninpaulinon@gmail.com>
2019-08-23 11:53:57 -0400

    Convert NavBar to Stateless Functional Component
    By now the NavBar rely only on the props provided by the App component, which leaves this component as a stateless component. With this transition we can work with a much more simpler syntax for such a simple component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Counter App{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Counter App
+        <span className="badge badge-pill badge-secondary m-4">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
